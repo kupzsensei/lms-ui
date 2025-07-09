dd1d671a79ac4f2aa3b47b3e7851b035905331c7
kupzsensei <kupzsensei@gmail.com>
2025-07-09 21:34:32 +0800

    final

--- src/api/section.js
+++ src/api/section.js
@@ -1,7 +1,13 @@
 import { BASE_URL } from ".";
 
 export const getSectionAPI = async () => {
-  const response = await fetch(`${BASE_URL}/api/sections/`);
+  const response = await fetch(`${BASE_URL}/api/sections/`, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyMDA3ODc2LCJpYXQiOjE3NTE5MjE0NzYsImp0aSI6IjRhZDdlY2NlYThiNDQ1NjJiOTNlYTNjNDhlMzBlZDBhIiwidXNlcl9pZCI6MiwidXNlcm5hbWUiOiJyYWxwIiwiZS1tYWlsIjoicmFscEBnbWFpbC5jb20iLCJsYXN0bmFtZSI6IkJlcm5hcmRvIiwicm9sZXMiOlsiVGVhY2hlciJdLCJwZXJzbWlzc2lvbnMiOlsic3ViamVjdC5kZWxldGVfc3ViamVjdCJdfQ.QzTNuqd_mGdin7t4_DdvNRnV5S__hSTYAPOLqUD1uok",
+      "Content-Type": "application/json",
+    },
+  });
   console.log("getSectionAPI is running");
 
   return await response.json();
@@ -31,7 +37,9 @@ export const updateSectionAPI = async ({ formData, accessToken, id }) => {
   const response = await fetch(`${BASE_URL}/api/sections/${id}/`, {
     method: "PATCH",
     headers: {
-      //   Authorization: `Bearer ${accessToken}`,
+      // Authorization: `Bearer ${accessToken}`,
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyMDA3ODc2LCJpYXQiOjE3NTE5MjE0NzYsImp0aSI6IjRhZDdlY2NlYThiNDQ1NjJiOTNlYTNjNDhlMzBlZDBhIiwidXNlcl9pZCI6MiwidXNlcm5hbWUiOiJyYWxwIiwiZS1tYWlsIjoicmFscEBnbWFpbC5jb20iLCJsYXN0bmFtZSI6IkJlcm5hcmRvIiwicm9sZXMiOlsiVGVhY2hlciJdLCJwZXJzbWlzc2lvbnMiOlsic3ViamVjdC5kZWxldGVfc3ViamVjdCJdfQ.QzTNuqd_mGdin7t4_DdvNRnV5S__hSTYAPOLqUD1uok",
       "Content-Type": "application/json",
     },
     body: JSON.stringify(formData),
--- src/pages/dashbord/sidebar.jsx
+++ src/pages/dashbord/sidebar.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "sirbenj-jwt-auth-client";
 
 export default function Sidebar() {
+  const { getRoles } = useAuth();
+  console.log(getRoles(), "roles");
   return (
     <nav className="flex flex-col gap-5 border-r border-gray-500 p-5 w-[200px]">
       <h1 className="font-bold text-2xl">LMS</h1>
@@ -10,7 +13,9 @@ export default function Sidebar() {
         {/* <NavLink to={"/"}>My Attendance</NavLink> */}
         <NavLink to={"/sections"}>Sections</NavLink>
         <NavLink to={"/subjects"}>Subjects</NavLink>
-        <NavLink to={"/activity"}>Activity</NavLink>
+        {["iAmATeacher"].includes("Teacher") ? (
+          <NavLink to={"/subjects"}>Activity</NavLink>
+        ) : null}
         {/* <NavLink to={"/"}>Dashborad</NavLink> */}
       </div>
     </nav>
